d50ed50692daa8c767e0ffba970856e3cc2a8b6e
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 19:05:04 +0000

    Initial Commit

---
+++ TabViewsDemo/TabViewsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewsDemoApp.swift
+//  TabViewsDemo
+//
+//  Created by Azu on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TabViewsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
